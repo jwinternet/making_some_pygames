d31349cd5f6e3e24aa1a9ca6a00438d1fa322dc3
jwinternet <47834941+Killface0@users.noreply.github.com>
2021-02-21 18:34:10 -0500

    Created a test pygame program that's basically a blank screen that does nothing!

---
+++ blankpygame.py
@@ -0,0 +1,17 @@
+"""
+blankpygame.py
+"""
+
+import pygame
+import sys
+from pygame.locals import *
+
+
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Hello PyGame World!")
+while True:
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit()
